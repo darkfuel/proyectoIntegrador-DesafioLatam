14427eeed7c26c6a3f9c609fe040cf9c9991733d
Constanza Pérez <constanza.vp.o@gmail.com>
2024-08-26 11:06:21 -0400

    inicialización del proyecto desde el front y back

---
+++ Backend/database/DML.sql
@@ -0,0 +1,32 @@
+-- Tabla para Usuarios (Clientes)
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    direccion VARCHAR(255) NOT NULL,
+    telefono VARCHAR(20),
+    is_admin BOOLEAN NOT NULL DEFAULT false
+);
+
+-- Tabla para Productos
+CREATE TABLE productos (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    descripcion TEXT,
+    precio INT NOT NULL,
+    img VARCHAR(1000) NOT NULL,
+    stock INT NOT NULL,
+    likes INT,
+    creado_por INT NOT NULL REFERENCES usuarios(id),
+    fecha_creacion timestamp with time zone default now()
+    );
+
+CREATE TABLE compras(
+    id SERIAL PRIMARY KEY,
+    id_usuario INT NOT NULL REFERENCES usuarios(id),
+    id_producto INT NOT NULL REFERENCES productos(id),
+    cantidad INT NOT NULL,
+    fecha_de_compra timestamp with time zone default now()
+)
